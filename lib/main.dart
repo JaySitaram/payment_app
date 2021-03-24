1be9991941de252f008ba957ebd502a7236b9142
jaymahaab <jaykadam913@gmail.com>
2021-03-24 22:21:24 +0530

    hive setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:rekur_fluter/pages/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory directory = await getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
+  Hive.openBox("Transaction");
   runApp(MyApp());
 }
 
